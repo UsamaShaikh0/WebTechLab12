c712e7c9e7c633f5a62985c6df1e0f265fb7e652
DaniyaShaikh <36057958+DaniyaShaikh@users.noreply.github.com>
2019-02-20 12:03:11 -0800

    Add files via upload

---
+++ Lab12/php-beginner-crud-level-1/database/insert_products.sql
@@ -0,0 +1,6 @@
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `created`, `modified`) VALUES
+(1, 'The Fault in our Starts', 'A Story of cancer patients.', 49.99, '2019-02-20 12:04:03', '2019-02-20 06:59:18'),
+(3, 'After You', 'Second part of Me Before you by Jojo Moyes.', 1.99, '2019-02-20 12:14:29', '2019-02-20 06:59:18'),
+(4, 'Subtle Art', 'Motivational Novel.', 6, '2019-02-20 12:15:04', '2019-02-20 06:59:18'),
+(5, 'All the bright places', 'by Jennifer Niven', 3.95, '2019-02-20 12:16:08', '2019-02-20 06:59:18'),
+(6, 'Me Before You', 'The book was first published on 5 January 2012 in ...', 99, '2019-02-20 12:16:08', '2019-02-20 06:59:18'),
